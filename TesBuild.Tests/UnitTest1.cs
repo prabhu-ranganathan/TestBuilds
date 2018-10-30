ae03bc92c79b5933dfbd6758138bca24277becee
Prabhu Ranganathan <Prabhu.Ranganathan@marlabs.com>
2018-10-30 17:43:53 +0530

    Clean Build

--- TesBuild.Tests/UnitTest1.cs
+++ TesBuild.Tests/UnitTest1.cs
@@ -6,6 +6,6 @@ namespace TesBuild.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1() => Assert.True(false1);
+        public void Test1() => Assert.True(false);
     }
 }
